e6d41b3556dab1e4a5fc2bf38eeb83df789aed3f
amanoperation-star <aman.operation@aman.eg>
2026-09-20 02:11:49 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,3 +20,10 @@ export default defineConfig(() => {
     },
   };
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/mo/', // أضف هذا السطر مع اسم مستودعك بالضبط
+})
